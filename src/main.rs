44b6c6982df49fb6f82ef609f5bda15a38691f5d
Samuel Carlsson <samuel.carlsson@gmail.com>
2024-03-27 09:22:42 +0100

    write image with cairo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+
+use cairo::{Context, ImageSurface, IoError};
+
+#[derive(Debug)]
+enum MapsError {
+    IoError(std::io::Error),
+    CairoError(cairo::Error),
+    CairoIoError(IoError),
+}
+
+impl From<std::io::Error> for MapsError {
+    fn from(error: std::io::Error) -> Self { MapsError::IoError(error) }
+}
+impl From<cairo::Error> for MapsError {
+    fn from(error: cairo::Error) -> Self { MapsError::CairoError(error) }
+}
+impl From<cairo::IoError> for MapsError {
+    fn from(error: cairo::IoError) -> Self { MapsError::CairoIoError(error) }
+}
+
+fn main() -> Result<(), MapsError> {
+    let surface = ImageSurface::create(cairo::Format::Rgb24, 640, 480)?;
+    let ctx = Context::new(&surface)?;
+    ctx.set_source_rgb(1.0, 1.0, 1.0);
+    ctx.paint()?;
+    let mut file = File::create("output.png")?;
+    surface.write_to_png(&mut file)?;
+    Ok(())
 }
